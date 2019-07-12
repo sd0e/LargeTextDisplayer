c77049b2e6c50d429721a331eb754d7f9311eaad
Sebastian Doe <sebastiandoe5@gmail.com>
2019-07-12 19:09:36 +0100

    Fix textbox finding

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function addCookie(){
-    window.location.href = "https://sebastiandoe5.github.io/LargeTextDisplayer/display.html?text=" + encodeURIComponent(document.getElementById("text").innerHTML);;
+    window.location.href = "https://sebastiandoe5.github.io/LargeTextDisplayer/display.html?text=" + encodeURIComponent(document.getElementById("text").value);;
 }
 
 function loadLocalStorage(){
     var LargeTextText = getURLParameter("text");
-    document.getElementById("marquee").innerHTML = LargeTextText;
+    document.getElementById("marquee").value = LargeTextText;
 }
