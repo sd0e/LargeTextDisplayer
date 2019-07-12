a789502181dd06dfbb6beaef6eeb837652cfbac9
Sebastian Doe <sebastiandoe5@gmail.com>
2019-07-12 19:03:33 +0100

    try

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function addCookie(){
-    var url = "https://sebastiandoe5.github.io/LargeTextDisplayer/display.html?text=" + encodeURIComponent(document.getElementById("text").innerHTML));
-    window.location.href = url;
+    window.location.href = "https://sebastiandoe5.github.io/LargeTextDisplayer/display.html?text=" + encodeURIComponent(document.getElementById("text").innerHTML));
 }
 
 function loadLocalStorage(){
-
     var LargeTextText = getURLParameter("text");
     document.getElementById("marquee").innerHTML = LargeTextText;
 }
