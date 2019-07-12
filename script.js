a8883c5bd82fa7d1eb3a72b36f30a2f963d180e3
Sebastian Doe <sebastiandoe5@gmail.com>
2019-07-12 20:22:02 +0100

    up my etiquette

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function addCookie(){
+function addCookie() {
     var url = "display.html?text=" + encodeURIComponent(document.getElementById("text").value) + "&foreground=" + encodeURIComponent(document.getElementById("foreground").value) + "&background=" + encodeURIComponent(document.getElementById("background").value);
     window.location.href = url;
 }
 
-function loadLocalStorage(){
+function loadLocalStorage() {
     var LargeTextText = getURLParameter("text");
     var LargeTextForeground = getURLParameter("foreground")
     var LargeTextBackground = getURLParameter("background")
