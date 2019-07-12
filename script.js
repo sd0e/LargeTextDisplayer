04e30733abe35f03caa50b6e63ce0505b4fd9cd6
Sebastian Doe <sebastiandoe5@gmail.com>
2019-07-12 20:12:50 +0100

    Try

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function addCookie(){
-    window.location.href = "https://sebastiandoe5.github.io/LargeTextDisplayer/display.html?text=" + encodeURIComponent(document.getElementById("text").value);;
+    var url = "display.html?text=" + encodeURIComponent(document.getElementById("text").value); + "&foreground=" + encodeURIComponent(document.getElementById("foreground").value); + "&background=" + encodeURIComponent(document.getElementById("background").value);;
+    window.location.href = url;
 }
 
 function loadLocalStorage(){
     var LargeTextText = getURLParameter("text");
+    var LargeTextForeground = getURLParameter("foreground")
+    var LargeTextBackground = getURLParameter("background")
     document.getElementById("marquee").innerHTML = LargeTextText;
+    document.getElementById("background").style.color = "#" + LargeTextForeground;
+    document.getElementById("background").style.backgroundColor = "#" + LargeTextBackground;
 }
